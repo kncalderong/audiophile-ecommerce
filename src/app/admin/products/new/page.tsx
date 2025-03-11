5cfd39ce1e6f8d71d5f5e9a3e7d85d13bcda19ad
Kevin Nicolas <kncalderong@gmail.com>
2025-03-11 13:02:08 -0500

    feature: create products form, route and functions developed

---
+++ src/app/admin/products/new/page.tsx
@@ -0,0 +1,10 @@
+import { ProductForm } from "../_components/ProductForm";
+
+export default function NewProductPage() {
+  return (
+    <>
+      <h1>Add Product</h1>
+      <ProductForm />
+    </>
+  );
+}
--- src/helpers/CRUD/product.ts
+++ src/helpers/CRUD/product.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { createClient } from "@/lib/supabase/server";
-import { Product } from "@prisma/client";
+import { DeviceType, Product } from "@prisma/client";
 import { PostgrestError } from "@supabase/supabase-js";
 
 export const deleteProduct = async (id: string) => {
@@ -21,6 +21,18 @@ export const updateProduct = async (
   if (error) throw new Error(error.message);
 };
 
+export const createProduct = async (data: {
+  [key: string]: unknown;
+}): Promise<Product | null> => {
+  const supabase = await createClient();
+  const { data: newProduct, error } = await supabase
+    .from("Product")
+    .insert([data]);
+
+  if (error) throw new Error(error.message);
+  return newProduct;
+};
+
 export const getAllProducts = async ({
   resultColumns = "*",
 }: {
@@ -37,4 +49,42 @@ export const getAllProducts = async ({
   if (error || !data)
     throw new Error(error?.message || "Failed to fetch categories");
   return data;
+};
+
+export const getNextImageOrder = async (productId: string) => {
+  const supabase = await createClient();
+  const { data, error } = await supabase
+    .from("ProductImage")
+    .select("order")
+    .eq("product_id", productId)
+    .order("order", { ascending: false }) // Get highest order first
+    .limit(1);
+
+  if (error) throw new Error(error.message);
+
+  return data?.length ? data[0].order + 1 : 1; // If no images, start from 1
+};
+
+export const addProductImage = async (
+  productId: string,
+  file: File,
+  deviceType: DeviceType
+) => {
+  const supabase = await createClient();
+  const nextOrder = await getNextImageOrder(productId); // Auto-assign order
+
+  const { data, error } = await supabase.storage
+    .from("product-images")
+    .upload(`products/${productId}/${file.name}`, file);
+
+  if (error) {
+    console.error("Upload error:", error);
+    return null;
+  }
+
+  const imageUrl = `https://${process.env.SUPABASE_URL}/storage/v1/object/public/product-images/${data.path}`;
+
+  await supabase
+    .from("ProductImage")
+    .insert([{ productId, imageUrl, order: nextOrder, deviceType }]);
 };
