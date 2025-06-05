db36074253f0dead3d60408ba3f954dcc1d027cd
Kevin Nicolas <kncalderong@gmail.com>
2025-06-05 10:11:19 -0500

    style: categories section styled on tablet and desktop

--- src/components/home/Categories.tsx
+++ src/components/home/Categories.tsx
@@ -10,14 +10,15 @@ const CATEGORIES = {
 
 export default function Categories() {
   return (
-    <section className="w-full bg-white py-28">
-      <div className="container custom-box flex flex-col gap-20">
+    <section className="w-full bg-white py-28 md:pt-36">
+      <div className="container custom-box grid grid-cols-1 gap-20 md:flex-row md:gap-2 md:grid-cols-3 lg:gap-8">
         {Object.entries(CATEGORIES).map(([key, value]) => (
-          <div
+          <Link
+            href={`/products/${key}`}
             key={key}
-            className="rounded-lg bg-store-gray-mid flex flex-col justify-end items-center h-[165px] relative p-2"
+            className="rounded-lg bg-store-gray-mid flex flex-col justify-end items-center h-[165px] relative p-2 w-full lg:h-[205px] group"
           >
-            <div className="w-[140px] aspect-square absolute -top-1/3 left-1/2 block translate-x-[-50%]">
+            <div className="w-[140px] aspect-square absolute -top-1/3 left-1/2 block translate-x-[-50%] lg:w-[200px] group-hover:-translate-y-3 ease-linear duration-300">
               <Image
                 src={value}
                 alt={key}
@@ -28,11 +29,9 @@ export default function Categories() {
             </div>
             <div className="flex flex-col gap-2 items-center">
               <h6 className="uppercase">{key}</h6>
-              <CustomButton variant="outline">
-                <Link href={`/products/${key}`}>shop</Link>
-              </CustomButton>
+              <CustomButton variant="outline">shop</CustomButton>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </section>
