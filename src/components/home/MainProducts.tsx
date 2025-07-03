a0ec0347a196e2d14b130f5ec3f48baaf9fed151
Kevin Nicolas <kncalderong@gmail.com>
2025-07-03 14:51:49 -0500

    feat: second and third products added to home page

--- src/components/home/MainProducts.tsx
+++ src/components/home/MainProducts.tsx
@@ -6,6 +6,14 @@ const firstProductMobile = "/home/mobile/image-speaker-zx9.png";
 const firstProductTablet = "/home/tablet/image-speaker-zx9.png";
 const firstProductDesktop = "/home/desktop/image-speaker-zx9.png";
 
+const secondProductMobile = "/home/mobile/image-speaker-zx7.jpg";
+const secondProductTablet = "/home/tablet/image-speaker-zx7.jpg";
+const secondProductDesktop = "/home/desktop/image-speaker-zx7.jpg";
+
+const thirdProductMobile = "/home/mobile/image-earphones-yx1.jpg";
+const thirdProductTablet = "/home/tablet/image-earphones-yx1.jpg";
+const thirdProductDesktop = "/home/desktop/image-earphones-yx1.jpg";
+
 export default function MainProducts() {
   return (
     <section className="w-full bg-white pb-28">
@@ -16,7 +24,7 @@ export default function MainProducts() {
             <div className="absolute top-1/2 left-1/2 border border-store-gray-mid rounded-full w-[210%] aspect-square -translate-x-1/2 -translate-y-1/2 opacity-40 z-0 md:w-[260%] lg:w-[110%]"></div>
             <div className="absolute top-1/2 left-1/2 border border-store-gray-mid rounded-full w-[360%] aspect-square -translate-x-1/2 -translate-y-1/2 opacity-40 z-0 md:w-[450%] lg:w-[200%]"></div>
             <PictureComponent
-              alt="Banner"
+              alt="ZX9-Speaker"
               mobile={firstProductMobile}
               tablet={firstProductTablet}
               desktop={firstProductDesktop}
@@ -34,6 +42,40 @@ export default function MainProducts() {
               variant={"secondary"}
               className="bg-black text-store-gray-mid z-10 hover:bg-gray-700 border-none"
             >
+              <Link href="#">See Product</Link>
+            </CustomButton>
+          </div>
+        </div>
+        <div className="w-full rounded-lg aspect-square relative block md:max-h-[320px]">
+          <PictureComponent
+            alt="ZX7-Speaker"
+            mobile={secondProductMobile}
+            tablet={secondProductTablet}
+            desktop={secondProductDesktop}
+          />
+          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col w-full gap-6 px-6 items-start md:px-12 lg:px-20">
+            <h3 className="w-full  md:text-h2 md:w-1/3 lg:text-5xl">
+              ZX7 SPEAKER
+            </h3>
+            <CustomButton variant={"secondary"} className="">
+              <Link href="#">See Product</Link>
+            </CustomButton>
+          </div>
+        </div>
+        <div className="w-full grid grid-cols-1 gap-6 md:grid-cols-2">
+          <div className="w-full rounded-lg relative block h-[200px] overflow-hidden md:h-[320px]">
+            <PictureComponent
+              alt="YX1-Earphones"
+              mobile={thirdProductMobile}
+              tablet={thirdProductTablet}
+              desktop={thirdProductDesktop}
+            />
+          </div>
+          <div className="w-full rounded-lg relative h-[200px] bg-store-gray-mid flex flex-col gap-6 px-6 items-start justify-center md:h-[320px]">
+            <h3 className="w-full  md:text-h2 md:w-1/3 lg:text-5xl">
+              YX1 EARPHONES
+            </h3>
+            <CustomButton variant={"secondary"} className="">
               <Link href="#">See Product</Link>
             </CustomButton>
           </div>
